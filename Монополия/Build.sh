5c8a65ccaebf11e6d48aa26684cb459e3e962e7c
AntonyanNarek <91913551+AntonyanNarek@users.noreply.github.com>
2021-10-13 09:14:28 +0200

    Update Build.sh

--- Монополия/Build.sh
+++ Монополия/Build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Компиляция исходников
-
 javac -sourcepath ./src -classpath ./lib/logback-core-1.2.6.jar:./lib/logback-classic-1.2.6.jar:./lib/slf4j-api-1.7.32.jar -d bin ./src/edu/csf/oop/monopoly/Main.java
 
 jar --create --file monopoly.jar --manifest .src/META-INF/MANIFEST.mf -C ./bin/ .
